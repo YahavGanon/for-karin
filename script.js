7f507631db8a2e0d9a3f0f020738e1937fb8a88e
YahavGanon <yahavganon86@gmail.com>
2024-04-29 16:01:18 +0300

    For my girl ( nice way of asking my girlfriend to go out with me on a date  )

---
+++ script.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", function() {
+    
+    const yesButton = document.querySelector('button')
+
+    const img = document.querySelector('.img-ask')
+
+    yesButton.addEventListener('click', function() {
+
+        img.src = "https://i.pinimg.com/originals/79/5a/54/795a5480fa173887a55ddc5d380ac9aa.gif"
+    })
+
+    const noButton = document.querySelector('.no-button')
+
+    noButton.addEventListener('mouseenter', function() {
+
+        const windowWidth = window.innerWidth
+        const windowHeight = window.innerHeight
+
+        const randomX = Math.floor(Math.random() * windowWidth)
+        const randomY = Math.floor(Math.random() * windowHeight)
+
+        noButton.style.position = 'absolute'
+        noButton.style.left = randomX + 'px'
+        noButton.style.top = randomY + 'px'
+    })
+})
+
+
+
+
